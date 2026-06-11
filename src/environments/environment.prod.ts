1c50e9a549de2aa56480ea36f766a825386b4a95
Irene Rossetti <irene.rossetti.j@gmail.com>
2026-06-11 01:38:32 -0400

    fix: forzar subida de environments reales para producción

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://parcial-1-sw-backend-ulz4.onrender.com/api',
+  mlServiceUrl: 'https://segundo-parcial-sw1-backend.onrender.com/api',
+  mlServiceWsUrl: 'wss://segundo-parcial-sw1-backend.onrender.com/api',
+  openaiApiKey: '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'
+};
