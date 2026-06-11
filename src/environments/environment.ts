29237eb9b24581d5298af9e8cdf42b9a45e79a8e
Irene Rossetti <irene.rossetti.j@gmail.com>
2026-06-11 09:55:42 -0400

    chore: include environment.ts in git to fix Vercel CI build

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api',
+  mlServiceUrl: 'http://localhost:8001/api',
+  mlServiceWsUrl: 'ws://localhost:8001/api',
+  openaiApiKey: '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'
+};
